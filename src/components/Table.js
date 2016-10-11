da2a283339c749575342ac604219c4f9c081c2e0
rauliyohmc <rauliyohmc@gmail.com>
2016-10-11 13:23:45 +0200

    Delegate Table rendering to small render functions

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,30 +1,37 @@
 import React from 'react';
 import '../styles/Posts.css';
 
-const renderHeader = (header) => (
-  <th key={header} className="table__header">
-    <span>{header}</span>
-    <span className="fa fa-arrow-down table__header-icon" aria-hidden="true" />
-  </th>
-);
-
-const Table = (props) => {
+const renderTableHeader = () => {
   const headers = ['ID', 'User name', 'Post title', 'Views', 'Likes', 'Created at'];
+  return (
+    <tr>
+      {headers.map(header => (
+        <th key={header} className="table__header">
+          <span>{header}</span>
+          <span className="fa fa-arrow-down table__header-icon" aria-hidden="true" />
+        </th>
+      ))}
+    </tr>
+  )
+};
+
+const renderTableBody = (posts, username) =>
+  posts.map(post => {
+    return (
+      <tr key={post.id} className={username === post.username ? 'table__row--active' : null}>
+        {Object.keys(post).map((key, i) => <td key={`${key}${i}`}>{post[key]}</td>)}
+      </tr>
+    )
+  });
+
+const Table = ({ posts, username }) => {
   return (
     <table className="table table-bordered table--white">
       <thead>
-        <tr>
-          {headers.map(header => renderHeader(header))}
-        </tr>
+        {renderTableHeader()}
       </thead>
       <tbody>
-      {props.posts.map(post => {
-        return (
-          <tr key={post.id} className={props.username === post.username ? 'table__row--active' : null}>
-            {Object.keys(post).map((key, i) => <td key={`${key}${i}`}>{post[key]}</td>)}
-          </tr>
-        )
-      })}
+        {renderTableBody(posts, username)}
       </tbody>
     </table>
   )
